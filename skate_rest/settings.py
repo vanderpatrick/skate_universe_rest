ea6ba838820f5809640ad7d71112da6555b77c0a
vanderpatrick <patrick123321123@icloud.com>
2022-10-27 10:46:44 +0000

    Install django REST and create super user

--- skate_rest/settings.py
+++ skate_rest/settings.py
@@ -20,6 +20,7 @@ CLOUDINARY_STORAGE = {
     'CLOUDINARY_URL': os.environ.get('CLOUDINARY_URL')
 }
 MEDIA_URL = '/media/'
+DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
