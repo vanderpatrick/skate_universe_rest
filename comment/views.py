4e88542d7b22525511f9689c01e84a7d4844e2b6
vanderpatrick <patrick123321123@icloud.com>
2022-10-31 12:00:15 +0000

    Install deployment dependencies and jwt tokens

--- comment/views.py
+++ comment/views.py
@@ -1,6 +1,7 @@
 from rest_framework import generics, permissions
 from skate_rest.permissions import IsOwnerOrReadOnly
 from .models import Comment
+from django_filters.rest_framework import DjangoFilterBackend
 from .serializers import CommentSerializer, CommentDetailSerializer
 
 
@@ -8,6 +9,8 @@ class CommentListView(generics.ListCreateAPIView):
     serializer_class = CommentSerializer
     permission_classes = [permissions.IsAuthenticatedOrReadOnly]
     queryset = Comment.objects.all()
+    filter_backends = [DjangoFilterBackend]
+    filterset_fields = ['post']
 
     def perform_create(self, serializer):
         serializer.save(author=self.request.user)
--- skate_rest/urls.py
+++ skate_rest/urls.py
@@ -19,6 +19,9 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls')),
+    path('dj-rest-auth/', include('dj_rest_auth.urls')),
+    path('dj-rest-auth/registration/', include(
+        'dj_rest_auth.registration.urls')),
     path('', include('Profile.urls')),
     path('', include('Post.urls')),
     path('', include('comment.urls')),
