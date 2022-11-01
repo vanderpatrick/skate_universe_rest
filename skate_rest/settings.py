c939a690cf1f6977bdaf2a662bd19e9cf75d01da
vanderpatrick <patrick123321123@icloud.com>
2022-11-01 12:52:37 +0000

    Set debug to false

--- skate_rest/settings.py
+++ skate_rest/settings.py
@@ -34,7 +34,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = 'DEV' in os.environ
+DEBUG = False
 
 ALLOWED_HOSTS = ['skate-universe-django-rest.herokuapp.com', 'localhost']
 
